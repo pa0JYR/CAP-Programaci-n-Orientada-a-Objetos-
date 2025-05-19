b00e77ef0c47decfa2d0cc0e16d4aa4db03068d2
Paola Yebra <paola.yebra@axity.com>
2025-05-19 14:23:02 -0600

    Actualizacion del archivo del  principio D

--- Principios SOLID/D.ts
+++ Principios SOLID/D.ts
@@ -1,1 +1,87 @@
-/* La clase padre no debe de conocer / depender de la clase */
+/** La clase padre no debe de conocer / depender de la clase 
+ * Proceso batch
+ * API rest 
+*/
+// Aplicando el principio
+interface Database {
+  insert(): void;
+  update(): void;
+  delete(): void;
+  select(): void;
+}
+
+class MySQLDatabase implements Database {
+  insert(): void {
+    // Lógica para insertar en MySQL
+  }
+
+  update(): void {
+    // Lógica para actualizar en MySQL
+  }
+
+  delete(): void {
+    // Lógica para eliminar en MySQL
+  }
+
+  select(): void {}
+}
+
+class MongoDB implements Database {
+  insert(): void {
+    // Lógica para insertar en MongoDB
+  }
+
+  update(): void {
+    // Lógica para actualizar en MongoDB
+  }
+
+  delete(): void {
+    // Lógica para eliminar en MongoDB
+  }
+
+  select(): void {}
+}
+
+class ElasticSearchDB implements Database {
+  insert(): void {
+    // Lógica para insertar en MongoDB
+  }
+
+  update(): void {
+    // Lógica para actualizar en MongoDB
+  }
+
+  delete(): void {
+    // Lógica para eliminar en MongoDB
+  }
+
+  select(): void {}
+}
+
+class BudgetReport {
+  private database: Database;
+
+  constructor(database: Database) {
+    this.database = database;
+  }
+
+  open(date: Date): void {
+    // Lógica para abrir el reporte
+  }
+
+  save(): void {
+    this.database.insert();
+  }
+
+  getData() {
+    return this.database.select();
+  }
+}
+
+const handlerSelectData = (typeDB: number) => {
+  // const mongDB = new MongoDB();
+  // const elastic = new ElasticSearchDB();
+  const mysql = new MySQLDatabase();
+  const budgetReport = new BudgetReport(mysql);
+  return budgetReport.getData();
+};
