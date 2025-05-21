5bce6662ca55db5058da982c8a8e0b09bf6616ee
Paola Yebra <paola.yebra@axity.com>
2025-05-20 19:37:29 -0600

    Creacion de la clase efectivo y arreglo del tipo efectivo

--- Ejercicio.ts
+++ Ejercicio.ts
@@ -26,15 +26,15 @@ interface IProducto {
 }
 /* Para fines practicos las monedas serán catalogadas como billetes */
 interface IEfectivo{
-     valor : string;
+     valor : number;
     cantidad : number;
     cantidadDisponible () : void;
     gastoBilletes (cantidaGastada : number) : void;
 }
 class Efectivo implements IEfectivo{
-    valor : string;
+    valor : number;
     cantidad : number;
-    constructor(valor : string, cantidad : number) {
+    constructor(valor : number, cantidad : number) {
         this.cantidad = cantidad;
         this.valor = valor;
     }
@@ -78,7 +78,7 @@ class MaquinaExpendedora {
     protected _dinero : Efectivo [] = [];
     protected _productos : Productos [] = [];
     
-    agregarBilletes (dineroDisponible : Efectivo []) : void{
+    agregarBilletes (dineroDisponible : Efectivo) : void{
         this._dinero.push(dineroDisponible);
     }
     agregarProducto (producto : Productos) : void{
@@ -95,7 +95,7 @@ class MaquinaExpendedora {
         if (!compra || compra.existencia <= 0) {
             console.log("No es posible realizar la compra");
         }else{
-            const  cambio : number = procesarPago(pago, compra.precio, this._dineroDisponible);
+            const  cambio : number = procesarPago(pago, compra.precio);
             switch (cambio) {
                 case -1:
                     console.log(`No es posible realizar la compra faltan: ${compra.precio - pago}`);
@@ -104,8 +104,8 @@ class MaquinaExpendedora {
                     console.log(`No hay suficiente cambio disponible. Intenta con un pago exacto.`);
                 default:
                     console.log(`Compra exitosa el cambio a dar es: ${cambio}`);
-                    this._dineroDisponible += compra.precio; 
-                    this._dineroDisponible -= cambio;  
+                    /* this._dineroDisponible += compra.precio; 
+                    this._dineroDisponible -= cambio;   */
                     compra.realizarCompra();
                     break;
             }
@@ -120,20 +120,43 @@ class MaquinaExpendedora {
         console.log(`Las ventas totales son: ${ventasTotales} pesos`);
     }
     mostrarDineroDisponible () : void{
-        console.log(`El dinero disponible actual es: ${this._dineroDisponible}`);
+        let total : number = 0;
+        this._dinero.forEach((b) => {
+            b.cantidadDisponible();
+            total = total + (b.cantidad * b.valor);
+        })
+        console.log(`La cantidad total es: ${total}`);
     }
 }
-function procesarPago (pago : number, precio : number, dineroDisponible : Efectivo []) : number {
+function procesarPago (pago : number, precio : number) : number {
     let cambio = pago - precio;
     if (cambio < 0) {
         return -1;
     }
-    if (cambio > dineroDisponible) {
+    /* if (cambio > dineroDisponible) {
         return -2
-    }
+    } */
     return cambio ;
 }
 const maquinaService = new MaquinaExpendedora ();
+const billeteCincuenta = new Efectivo (50,40);
+const billeteCien = new Efectivo (100,40);
+const billeteVinte = new Efectivo (20,40);
+const billeteDoscientos = new Efectivo (200,40);
+const billeteQuinientos = new Efectivo (500,40);
+const monedaUno = new Efectivo (1,40);
+const monedaDos = new Efectivo (2,40);
+const monedaCinco = new Efectivo (5,40);
+const monedaDiez = new Efectivo (10,40);
+maquinaService.agregarBilletes(billeteCien);
+maquinaService.agregarBilletes(billeteCincuenta);
+maquinaService.agregarBilletes(billeteDoscientos);
+maquinaService.agregarBilletes(billeteQuinientos);
+maquinaService.agregarBilletes(billeteVinte);
+maquinaService.agregarBilletes(monedaUno);
+maquinaService.agregarBilletes(monedaDos);
+maquinaService.agregarBilletes(monedaDiez);
+maquinaService.agregarBilletes(monedaCinco);
 const chicleBubalo = new Productos ('chicle', 'botana',3,'Bubalo', 20);
 const papasSabritas = new Productos ('papas','botana',15,'Sabritas',30);
 const jugoMango = new Productos ('Boing Mango', 'Jugo', 12,'Boing',20);
